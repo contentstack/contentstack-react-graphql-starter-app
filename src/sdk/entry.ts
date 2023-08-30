cd7ce7ade235c3e64181a8b3fa3900b6a1048128
Sairaj Chouhan <sairaj2119@gmail.com>
2023-08-30 18:25:39 +0530

    remove dead code

--- src/sdk/entry.ts
+++ src/sdk/entry.ts
@@ -1,23 +1,9 @@
-import * as Utils from "@contentstack/utils";
 import ContentstackLivePreview from "@contentstack/live-preview-utils";
 import {
   customHostUrl,
   initializeContentStackSdk,
   isValidCustomHostUrl,
 } from "./utils";
-
-type GetEntry = {
-  contentTypeUid: string;
-  referenceFieldPath: string[] | undefined;
-  jsonRtePath: string[] | undefined;
-};
-
-type GetEntryByUrl = {
-  entryUrl: string | undefined;
-  contentTypeUid: string;
-  referenceFieldPath: string[] | undefined;
-  jsonRtePath: string[] | undefined;
-};
 
 const {
   REACT_APP_CONTENTSTACK_API_HOST,
@@ -58,78 +44,4 @@ export const { onEntryChange } = ContentstackLivePreview;
 
 export const renderOption = {
   span: (node: any, next: any) => next(node.children),
-};
-
-/**
- *
- * fetches all the entries from specific content-type
- * @param {* content-type uid} contentTypeUid
- * @param {* reference field name} referenceFieldPath
- * @param {* Json RTE path} jsonRtePath
- *
- */
-export const getEntry = ({
-  contentTypeUid,
-  referenceFieldPath,
-  jsonRtePath,
-}: GetEntry) => {
-  return new Promise((resolve, reject) => {
-    const query = Stack.ContentType(contentTypeUid).Query();
-    if (referenceFieldPath) query.includeReference(referenceFieldPath);
-    query
-      .toJSON()
-      .find()
-      .then(
-        (result) => {
-          jsonRtePath &&
-            Utils.jsonToHTML({
-              entry: result,
-              paths: jsonRtePath,
-              renderOption,
-            });
-          resolve(result);
-        },
-        (error) => {
-          reject(error);
-        }
-      );
-  });
-};
-
-/**
- *fetches specific entry from a content-type
- *
- * @param {* content-type uid} contentTypeUid
- * @param {* url for entry to be fetched} entryUrl
- * @param {* reference field name} referenceFieldPath
- * @param {* Json RTE path} jsonRtePath
- * @returns
- */
-export const getEntryByUrl = ({
-  contentTypeUid,
-  entryUrl,
-  referenceFieldPath,
-  jsonRtePath,
-}: GetEntryByUrl) => {
-  return new Promise((resolve, reject) => {
-    const blogQuery = Stack.ContentType(contentTypeUid).Query();
-    if (referenceFieldPath) blogQuery.includeReference(referenceFieldPath);
-    blogQuery.toJSON();
-    const data = blogQuery.where("url", `${entryUrl}`).find();
-    data.then(
-      (result) => {
-        jsonRtePath &&
-          Utils.jsonToHTML({
-            entry: result,
-            paths: jsonRtePath,
-            renderOption,
-          });
-        resolve(result[0]);
-      },
-      (error) => {
-        console.error(error);
-        reject(error);
-      }
-    );
-  });
 };
