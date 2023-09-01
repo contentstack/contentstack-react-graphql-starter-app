7ad1eb07753ddcd389030888b21d32afd3508722
Sairaj Chouhan <sairaj2119@gmail.com>
2023-09-01 18:43:11 +0530

    add back config keys for live preview init

--- src/utils/live-preview.ts
+++ src/utils/live-preview.ts
@@ -1,10 +1,22 @@
 import ContentstackLivePreview from "@contentstack/live-preview-utils";
 
-const { REACT_APP_CONTENTSTACK_LIVE_PREVIEW } = process.env;
+const {
+  REACT_APP_CONTENTSTACK_LIVE_PREVIEW,
+  REACT_APP_CONTENTSTACK_APP_HOST,
+  REACT_APP_CONTENTSTACK_API_KEY,
+  REACT_APP_CONTENTSTACK_ENVIRONMENT,
+} = process.env;
 
 // Setting LP if enabled
 ContentstackLivePreview.init({
   enable: REACT_APP_CONTENTSTACK_LIVE_PREVIEW === "true",
+  clientUrlParams: {
+    host: REACT_APP_CONTENTSTACK_APP_HOST,
+  },
+  stackDetails: {
+    apiKey: REACT_APP_CONTENTSTACK_API_KEY,
+    environment: REACT_APP_CONTENTSTACK_ENVIRONMENT,
+  },
   ssr: false,
 })?.catch((err) => console.error(err));
 
